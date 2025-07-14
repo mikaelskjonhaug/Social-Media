53ba1a7f9dd29494db834c548b54a79d850faaf1
Mikael Skjonhaug <mikaelskjonhaug@gmail.com>
2025-07-14 11:52:22 -0700

    Initial Commit

---
+++ Social Media/Social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaApp.swift
+//  Social Media
+//
+//  Created by Mikael Skjonhaug on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
